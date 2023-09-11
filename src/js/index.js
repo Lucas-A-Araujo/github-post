c4323d99c5fbd020e93aa9d3335a6538cf23b862
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-09-11 15:21:19 -0300

    feat: handle form error

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ const apiUrl = 'https://7pu263mpcarw3lhazop5ec7u7e0bclzu.lambda-url.us-east-1.on
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('submitForm');
     const modalOverlay = document.querySelector(".modal-overlay");
+    const modalTitle = document.querySelector(".modal-content h2");
+    const modalContent = document.querySelector(".modal-content p");
     const closeModalButton = document.getElementById("closeModal");
 
     form.addEventListener('submit', function (e) {
@@ -21,17 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
             .then((response) => {
                 console.log(response);
                 if (response.ok) {
-                    displaySuccessModal();
+                    displayModal('Success!', 'Your form has been submitted successfully.');
                 } else {
                     console.error('Form submission failed.');
+                    displayModal('Oops!', 'Form submission failed.');
                 }
             })
             .catch((error) => {
                 console.error('An error occurred:', error);
+                displayModal('Oops!', 'Something went wrong.');
             });
     });
 
-    const displaySuccessModal = () => {
+    const displayModal = (title, content) => {
+        modalTitle.innerHTML = title;
+        modalContent.innerHTML = content;
+
         modalOverlay.style.display = "flex";
 
         closeModalButton.addEventListener("click", () => {
